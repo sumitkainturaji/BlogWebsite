d0e1cf68e5d616e0eee33e727ed26ce2618834d1
Sumit Singh kaintura <98005540+sumitkainturaji@users.noreply.github.com>
2025-03-30 17:57:57 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  important: true, // Add this to make Tailwind styles override defaults
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+
